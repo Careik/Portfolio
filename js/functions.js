1b5d8c7d74bfdfcdcb3feb55e7a755b70615218b
Eric Hellmuth <hellmuth07@gmail.com>
2020-07-22 15:11:04 -0400

    jquery fun

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,14 @@
-$(document).ready(function () {
-    var scroll_pos = 0;
-    $("#header").scroll(function () {
-        scroll_pos = $(this).scrollTop();
-        if (scroll_pos > 210) {
-            $("#header").css('background-color', '#1A1A1A');
-        } else {
-            $("#header").css('background-color', 'red');
-        }
-        console.log(scroll_pos);
-    });
-});
+{
+  $(document).ready(function(){
+              var scroll_pos = 0;
+              $(document).scroll(function() {
+                  scroll_pos = $(this).scrollTop();
+                  if(scroll_pos < $(window).height()) {
+                      $("#header").css('background', 'transparent');
+                  } else {
+                      $("#header").css('background 1s ease-in-out', 'rgba(36, 31, 32, 1.0)');
+                  }
+              });
+          });
+
+}
